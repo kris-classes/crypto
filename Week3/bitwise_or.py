894bd52c8ed05cecb035801a9ff21836d2111cae
Kris Pritchard <krp@users.noreply.github.com>
2021-03-18 15:00:23 +1300

    Add bitwise_or file

---
+++ Week3/bitwise_or.py
@@ -0,0 +1,18 @@
+"""
+Create a function named bitwise_or which takes two integers,
+prints the integer values, then performs a bitwise OR (different to a boolean OR),
+prints the result of the OR, then returns the result.
+
+NOTE: We can't call this function 'or' because that name is reserved in Python so we call it bitwise_or instead.
+"""
+from helper_functions import binary, FIX_ME
+
+"""
+Using code from the bitwise_and() function (see helper_functions.py), create an additional function called bitwise_or() which perform the bitwise OR operation (using the | character, also known as a pipe)
+"""
+def bitwise_or(integer_a, integer_b):
+    FIX_ME('Implement the bitwise_or() function')  # TODO: You can delete this line once you implement your function. 'pass' just tells Python to do nothing.
+
+
+if __name__ == '__main__':
+    bitwise_or(85, 170)
