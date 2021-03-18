3e1fb3476f05711313286c6fb07d3cc0e09bd397
Kris Pritchard <krp@users.noreply.github.com>
2021-03-18 15:05:12 +1300

    Add string_to_binary

---
+++ Week3/string_to_binary.py
@@ -0,0 +1,16 @@
+"""
+Complete the str_to_binary function.
+"""
+from helper_functions import binary, FIX_ME
+
+
+def str_to_binary(input_string):
+    # TODO: Add a loop here which for each loop, prints the binary string corresponding to each input character
+    # You will need to use 2 functions here. One to convert from character to integer, and another to convert from integer to binary.
+    FIX_ME('Implement str_to_binary()')
+
+
+
+
+if __name__ == '__main__':
+    str_to_binary('hello')  # Should print the line: 0b1101000 0b1100101 0b1101100 0b1101100 0b1101111
