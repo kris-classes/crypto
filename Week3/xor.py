3b364e1be0136fcd709fa394864c11fc72e5c6b8
Kris Pritchard <krp@users.noreply.github.com>
2021-03-18 15:14:47 +1300

    Add xor function

---
+++ Week3/xor.py
@@ -0,0 +1,18 @@
+"""
+Create a function named xor which takes two ascii codes,
+prints their binary, then performs a bitwise XOR using the ^ (caret) operator,
+prints the result of the XOR, then returns the result.
+"""
+from helper_functions import binary, FIX_ME
+
+"""
+Using code from the bitwise_and() function (see helper_functions.py), create an additional function called xor() which perform the XOR operation.
+"""
+def xor(integer_a, integer_b):
+    FIX_ME('Implement the xor() function')  # TODO: You can delete this line once you implement your function.
+
+
+
+if __name__ == '__main__':
+    xor(85, 170)
+
